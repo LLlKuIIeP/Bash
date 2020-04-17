c595cc74aef72eb90bda152f442bedcccdca1718
LLlKuIIeP <79032312801@yandex.ru>
2020-04-17 22:03:45 +0300

    test_pass

--- install_lamp.sh
+++ install_lamp.sh
@@ -8,3 +8,12 @@ echo -e "\n\n\tinstall phpmyadmin\n"
 sudo add-apt-repository ppa:phpmyadmin/ppa
 sudo apt-get update
 sudo apt-get install phpmyadmin -y
+
+echo -e "\n\n\tset password for root\n"
+cd ~/
+touch alter_pass_root.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1111';" > alter_pass_root.sql
+sudo mysql < alter_pass_root.sql
+exit
+rm -f alter_pass_root.sql
+
