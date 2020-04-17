32a69622619dd5bf7a00fc5768e6708aac754cf4
LLlKuIIeP <79032312801@yandex.ru>
2020-04-17 23:26:39 +0300

    composer

--- install_lamp.sh
+++ install_lamp.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
+sudo apt-get update
+
+#	Apache MySql PHP
 echo -e "\n\n\tinstall lamp\n"
 sudo apt install tasksel -y
 sudo tasksel install lamp-server
+
+echo -e "\n\n\tinstall modules for php\n"
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install -y php-bcmath php-curl php-db php-mbstring php-mysql php-bz2 php-xml
 
 echo -e "\n\n\tinstall phpmyadmin\n"
 sudo add-apt-repository ppa:phpmyadmin/ppa
 sudo apt-get update
 sudo apt-get install phpmyadmin -y
 
-
-#	https://qna.habr.com/q/535222
-#	https://stackoverflow.com/questions/49948350/phpmyadmin-on-mysql-8-0/49950165
-
-echo -e "\n\n\tset password for root\n"
+# https://qna.habr.com/q/535222
+# https://stackoverflow.com/questions/49948350/phpmyadmin-on-mysql-8-0/49950165
+echo -e "\n\n\tset password for user\n"
 cd ~/
 touch create_user.sql
 echo -e "CREATE USER 'user'@'localhost' IDENTIFIED WITH mysql_native_password BY '1111';\n" > create_user.sql
@@ -22,4 +28,15 @@ echo -e "FLUSH PRIVILEGES;" >> create_user.sql
 sudo mysql < create_user.sql
 exit
 rm -f create_user.sql
+#************************************************
 
+
+#       Composer
+echo -e "\n\n\tinstall Composer\n"
+sudo update
+sudo apt-get install -y wget php-cli php-zip unzip curl
+cd ~/
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+composer -v
+#************************************************
