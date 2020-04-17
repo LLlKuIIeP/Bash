cf832426d14fee7c32ff02e4c7b6e4f4aa696065
LLlKuIIeP <79032312801@yandex.ru>
2020-04-17 22:34:04 +0300

    user

--- install_lamp.sh
+++ install_lamp.sh
@@ -9,11 +9,17 @@ sudo add-apt-repository ppa:phpmyadmin/ppa
 sudo apt-get update
 sudo apt-get install phpmyadmin -y
 
+
+#	https://qna.habr.com/q/535222
+#	https://stackoverflow.com/questions/49948350/phpmyadmin-on-mysql-8-0/49950165
+
 echo -e "\n\n\tset password for root\n"
 cd ~/
-touch alter_pass_root.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '1111';" > alter_pass_root.sql
-sudo mysql < alter_pass_root.sql
+touch create_user.sql
+echo -e "CREATE USER 'user'@'localhost' IDENTIFIED WITH mysql_native_password BY '1111';\n" > create_user.sql
+echo -e "GRANT ALL PRIVILEGES ON * . * TO 'user'@'localhost';\n" >> create_user.sql
+echo -e "FLUSH PRIVILEGES;" >> create_user.sql
+sudo mysql < create_user.sql
 exit
-rm -f alter_pass_root.sql
+rm -f create_user.sql
 
