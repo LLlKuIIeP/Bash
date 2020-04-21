fb361dd3cb00914d8e9c4067c799f04d945d8f81
LLlKuIIeP <79032312801@yandex.ru>
2020-04-21 23:30:43 +0300

    fix

--- install_cdash_docker.sh
+++ install_cdash_docker.sh
@@ -5,7 +5,7 @@
 echo -e "\n\n\tInstall Docker\n"
 sudo apt-get update
 sudo apt-get remove docker docker-engine docker.io
-sudo apt install docker.io
+sudo apt install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
@@ -15,6 +15,7 @@ docker --version
 echo -e "\n\n\tInstall docker-ompose\n"
 sudo apt-get update
 sudo apt-get remove docker-compose
+sudo apt-get install -y jq
 VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 DESTINATION=/usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
