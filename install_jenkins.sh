439a5e053678f0c5a18eef10699fe9bee9db2222
LLlKuIIeP <79032312801@yandex.ru>
2020-04-26 22:46:16 +0300

    fix

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 
@@ -7,7 +6,7 @@ echo -e "\n\n\tInstall Jenkins\n"
 sudo apt-get update
 sudo apt-get install openjdk-8-jre -y
 java -version
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
