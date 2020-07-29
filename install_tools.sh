48a7149f5e291927bee11ed18ceca9a161f9fb6c
LLlKuIIeP <79032312801@yandex.ru>
2020-07-29 16:59:08 +0300

    Update install_tools.sh
    fix

--- install_tools.sh
+++ install_tools.sh
@@ -4,4 +4,4 @@
 
 #       Install Tools
 echo -e "\n\n\tInstall Tools\n"
-sudo apt install -y g++ make cmake vim net-tools libglu1-mesa-dev freeglut3-dev mesa-common-dev
+sudo apt install -y g++ make cmake vim net-tools libglu1-mesa-dev freeglut3-dev mesa-common-dev openssh-server
