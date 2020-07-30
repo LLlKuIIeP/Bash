dd191078a395d122dbcd223deced4a7dcbe77a58
LLlKuIIeP <79032312801@yandex.ru>
2020-07-30 22:24:10 +0300

    clang

---
+++ install_clang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#       Install Clang
+echo -e "\n\n\tInstall Clang\n"
+cd ~
+git clone https://github.com/llvm/llvm-project.git
+cd llvm-project
+mkdir build
+cd build
+sudo apt install -y python3-distutils
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lldb;compiler-rt;lld;polly;debuginfo-tests;llvm" -DCMAKE_BUILD_TYPE=Release ../llvm
+cmake --build . -j 8
+sudo make install
