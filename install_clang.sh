85b37ba946c4902eefa78621241fe1e89e942987
LLlKuIIeP <79032312801@yandex.ru>
2020-07-30 22:30:59 +0300

    iwyu

--- install_clang.sh
+++ install_clang.sh
@@ -12,3 +12,8 @@ sudo apt install -y python3-distutils
 cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;libcxx;libcxxabi;libunwind;lldb;compiler-rt;lld;polly;debuginfo-tests;llvm" -DCMAKE_BUILD_TYPE=Release ../llvm
 cmake --build . -j 8
 sudo make install
+
+
+#       Install Include what you use
+echo -e "\n\n\tInstall Include what you use\n"
+sudo apt install -y iwyu
