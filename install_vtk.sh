8c36471818c471579fc47bfd6c4f20a280c75b81
LLlKuIIeP <79032312801@yandex.ru>
2020-10-08 21:57:38 +0300

    Create install_vtk.sh

---
+++ install_vtk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir /home/user/vtk_build
+cd /home/user/vtk_build
+
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DCMAKE_INSTALL_PREFIX:STRING=/usr/local ../vtk_sources
+
+cmake --build . -j 8
+
+make install
