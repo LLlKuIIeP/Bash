b51464ce83c759b4798c13cac99453172ed8d087
LLlKuIIeP <79032312801@yandex.ru>
2020-10-12 10:47:48 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -6,7 +6,7 @@ echo -e "\n\n\tInstall Docker\n"
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl  gnupg-agent software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-add-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
