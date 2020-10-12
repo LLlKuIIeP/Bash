7965a421c7137b826776645653ad5a0be0dd714a
LLlKuIIeP <79032312801@yandex.ru>
2020-10-12 10:28:47 +0300

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,12 +3,12 @@
 
 #       Install Docker
 echo -e "\n\n\tInstall Docker\n"
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl  gnupg-agent software-properties-common
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl  gnupg-agent software-properties-common
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
@@ -16,9 +16,9 @@ docker --version
 
 #       Install Docker Compose
 echo -e "\n\n\tInstall docker-ompose\n"
-sudo apt-get update
-sudo apt-get remove docker-compose
-sudo apt-get install -y jq
+sudo apt update
+sudo apt remove docker-compose
+sudo apt install -y jq
 VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 DESTINATION=/usr/local/bin/docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
