2682f1a4f590198a2792ab7cf90ec4cdc37937f8
LLlKuIIeP <79032312801@yandex.ru>
2020-10-14 22:46:09 +0300

    Update install_boost.sh

--- install_boost.sh
+++ install_boost.sh
@@ -10,20 +10,21 @@ echo -e "\n\n\tInstall Boost\n"
 #git submodule init
 #git submodule update
 
-cd /home/user/boost
+#cd /home/user/boost
 #chmod +x tools/build/src/engine/build.sh
 
+cd /tmp/boost
 bash ./bootstrap.sh
 
-mkdir /opt/boost
-chmod a+rwx /opt/boost
+#mkdir /opt/boost
+#chmod a+rw /opt/boost
 
 ./b2 install
 
 
 #     Env
-sh -c "echo 'BOOST_ROOT=\"/opt/boost\"' >> /etc/environment"
-sh -c "echo 'BOOST_LIBRARYDIR=\"/opt/boost/lib\"' >> /etc/environment"
-sh -c "echo 'BOOST_INCLUDEDIR=\"/opt/boost/include\"' >> /etc/environment"
+#sh -c "echo 'BOOST_ROOT=\"/opt/boost\"' >> /etc/environment"
+#sh -c "echo 'BOOST_LIBRARYDIR=\"/opt/boost/lib\"' >> /etc/environment"
+#sh -c "echo 'BOOST_INCLUDEDIR=\"/opt/boost/include\"' >> /etc/environment"
 
-source /etc/environment
+#source /etc/environment
