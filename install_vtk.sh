bcf9f647bea39ebcc077b69ccff15b10c96c724f
LLlKuIIeP <79032312801@yandex.ru>
2020-11-06 21:16:26 +0300

    Update install_vtk.sh

--- install_vtk.sh
+++ install_vtk.sh
@@ -2,7 +2,7 @@
 
 mkdir /tmp/vtk_build
 
-cmake -S /tmp/vtk_sources -B /tmp/vtk_build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DCMAKE_INSTALL_PREFIX:STRING=/usr/local
+cmake -S /tmp/vtk_sources -B /tmp/vtk_build -G "Unix Makefiles" -DCMAKE_BUILD_TYPE:STRING=Release -DBUILD_SHARED_LIBS:BOOL=OFF -DBUILD_TESTING:BOOL=OFF -DBUILD_EXAMPLES:BOOL=OFF -DCMAKE_INSTALL_PREFIX:STRING=/usr/local -DCMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON
 
 cmake --build /tmp/vtk_build -j 8
 
